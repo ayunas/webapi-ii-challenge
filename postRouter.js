044012d738c877d51ccb02bffdb51cb04da2d1d4
ayunas <ayunas@gmail.com>
2019-09-03 16:50:20 -0500

    added get endpoint for comment functionality

--- postRouter.js
+++ postRouter.js
@@ -22,4 +22,17 @@ postRouter.get("/:id", (req, res) => {
     .catch(err => res.status(500).json(err.message));
 });
 
+postRouter.get("/:id/comments", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  db.findPostComments(id)
+    .then(comments => {
+      if (comments.length) {
+        res.status(200).json(comments);
+      }
+      res.status(404).json({ error: `comments for post ${id} not found` });
+    })
+    .catch(err => res.status());
+});
+
 module.exports = postRouter;
